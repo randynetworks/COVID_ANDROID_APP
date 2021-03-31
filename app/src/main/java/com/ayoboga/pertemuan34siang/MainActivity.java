7129ac6402f519270b764d82cce8d95bd140f63f
randynetworks <randyramadhan1973@gmail.com>
2021-03-31 11:52:07 +0700

    feat: add form

---
+++ app/src/main/java/com/ayoboga/pertemuan34siang/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ayoboga.pertemuan34siang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
