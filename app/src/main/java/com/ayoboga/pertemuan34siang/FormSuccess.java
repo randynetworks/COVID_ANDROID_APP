aa521fc95c6957b486ecbaf242ae822f3dfc53a7
randynetworks <randyramadhan1973@gmail.com>
2021-04-10 10:20:54 +0700

    feat: add some information form register, Feat: Widget calendar and radio button for jk

--- app/src/main/java/com/ayoboga/pertemuan34siang/FormSuccess.java
+++ app/src/main/java/com/ayoboga/pertemuan34siang/FormSuccess.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class FormSuccess extends AppCompatActivity {
 
     @Override
@@ -18,15 +20,25 @@ public class FormSuccess extends AppCompatActivity {
         setContentView(R.layout.form_success);
 
         String strNama = getIntent().getStringExtra("VAR_NAMA");
-        String strEmail = getIntent().getStringExtra("VAR_EMAIL");
+        String strNIK = getIntent().getStringExtra("VAR_NIK");
+        String strJK = getIntent().getStringExtra("VAR_JK");
+        String strDate = getIntent().getStringExtra("VAR_DATE");
         String strAlamat = getIntent().getStringExtra("VAR_ALAMAT");
+        String strEmail = getIntent().getStringExtra("VAR_EMAIL");
+
 
 
         TextView lblNama = (TextView) findViewById(R.id.nama_success);
-        TextView lblEmail = (TextView) findViewById(R.id.email_success);
+        TextView lblNIK = (TextView) findViewById(R.id.nik_success);
+        TextView lblJK = (TextView) findViewById(R.id.jk_success);
+        TextView lblTgl = (TextView) findViewById(R.id.tgl_success);
         TextView lblAlamat = (TextView) findViewById(R.id.alamat_success);
+        TextView lblEmail = (TextView) findViewById(R.id.email_success);
 
         lblNama.setText("Hi, " + strNama + "!");
+        lblNIK.setText("NIK kamu, " + strNIK + ",");
+        lblJK.setText("Dengan Jenis Kelamin " + strJK + ",");
+        lblTgl.setText("Dan Tanggal Lahir, " + strDate + ",");
         lblEmail.setText("Kamu Telah terdaftar Vaksinasi dengan email ini,\n" + strEmail);
         lblAlamat.setText("Vaksinasi akan berada di daerah alamat,\n" + strAlamat);
     }
--- app/src/main/java/com/ayoboga/pertemuan34siang/MainActivity.java
+++ app/src/main/java/com/ayoboga/pertemuan34siang/MainActivity.java
@@ -1,14 +1,29 @@
 package com.ayoboga.pertemuan34siang;
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private EditText displayDate;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
 
 
     @Override
@@ -16,10 +31,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_input);
 
+        displayDate = (EditText) findViewById(R.id.id_tgl);
+        displayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,R.style.Theme_AppCompat_Light_Dialog, dateSetListener , year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                dialog.show();
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + dayOfMonth + "/" + year);
+
+                String date = month + "/" + dayOfMonth + "/" + year;
+                displayDate.setText(date);
+            }
+        };
+
+
         Button btnSubmit = findViewById(R.id.buttonSubmit);
         btnSubmit.setOnClickListener(new MainActivity.submitForm());
-    }
 
+    }
 
     public class submitForm implements View.OnClickListener {
         @Override
@@ -27,15 +69,26 @@ public class MainActivity extends AppCompatActivity {
             EditText textNama = findViewById(R.id.id_name);
             EditText textEmail = findViewById(R.id.id_email);
             EditText textAlamat = findViewById(R.id.id_alamat);
+            EditText textNIK = findViewById(R.id.id_NIK);
+            EditText textDate = findViewById(R.id.id_tgl);
+            RadioGroup radioJK = findViewById(R.id.id_JK);
+            RadioButton JK = findViewById(radioJK.getCheckedRadioButtonId());
 
             String strNama = textNama.getText().toString();
             String strEmail = textEmail.getText().toString();
             String strAlamat = textAlamat.getText().toString();
+            String strNIK = textNIK.getText().toString();
+            String strDate = textDate.getText().toString();
+            String strJK = JK.getText().toString();
 
             Intent formSuccess = new Intent(MainActivity.this, FormSuccess.class);
             formSuccess.putExtra("VAR_NAMA", strNama);
-            formSuccess.putExtra("VAR_EMAIL", strEmail);
+            formSuccess.putExtra("VAR_NIK", strNIK);
+            formSuccess.putExtra("VAR_JK", strJK);
+            formSuccess.putExtra("VAR_DATE", strDate);
             formSuccess.putExtra("VAR_ALAMAT", strAlamat);
+            formSuccess.putExtra("VAR_EMAIL", strEmail);
+
             startActivity(formSuccess);
         }
     }
