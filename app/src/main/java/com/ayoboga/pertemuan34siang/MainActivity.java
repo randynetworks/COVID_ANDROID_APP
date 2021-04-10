340e60639b6f2fc6b75097f85c0e09b16a138b50
randynetworks <randyramadhan1973@gmail.com>
2021-04-10 13:01:49 +0700

    feat: add snackbar and alert dialog

--- app/src/main/java/com/ayoboga/pertemuan34siang/MainActivity.java
+++ app/src/main/java/com/ayoboga/pertemuan34siang/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ayoboga.pertemuan34siang;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -14,8 +16,11 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Calendar;
 
@@ -87,27 +92,55 @@ public class MainActivity extends AppCompatActivity {
             String strJK = JK.getText().toString();
             boolean status = sakit.isChecked();
 
+
+            Snackbar Informasi = Snackbar.make(v, "Data " + strNama +" tersimpan!", Snackbar.LENGTH_LONG);
+            Informasi.setAction("Lihat Detail", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Toast
+                    Toast data = Toast.makeText(getApplicationContext(), "Data : \n" +
+                                    "Nama\t\t\t" + strNama + "\n" +
+                                     "Data Input NIK\t\t\t" + strNIK + "\n" +
+                                    "Data Input JK\t\t\t" + strJK + "\n" +
+                                    "Data Input Date\t\t\t" + strDate + "\n" +
+                                    "Data Input Email\t\t\t" + strEmail + "\n" +
+                                    "Data Input Alamat\t\t" + strAlamat + "\n" +
+                                     "Data Input Status\t\t" + status
+                            , Toast.LENGTH_LONG);
+                    data.show();
+                }
+            });
+            Informasi.show();
+
             System.out.println("Data pada Variable :");
-            System.out.println("Data Input Nama\t\t\t" + strNama);
-            System.out.println("Data Input NIK\t\t\t" + strNIK);
-            System.out.println("Data Input JK\t\t\t" + strJK);
-            System.out.println("Data Input Date\t\t\t" + strDate);
-            System.out.println("Data Input Email\t\t\t" + strEmail);
-            System.out.println("Data Input Alamat\t\t" + strAlamat);
-            System.out.println("Data Input Status\t\t" + status);
+            System.out.println("Data Input Nama : " + strNama);
+            System.out.println("Data Input NIK : " + strNIK);
+            System.out.println("Data Input JK : " + strJK);
+            System.out.println("Data Input Date : " + strDate);
+            System.out.println("Data Input Email : " + strEmail);
+            System.out.println("Data Input Alamat : " + strAlamat);
+            System.out.println("Data Input Status : " + status);
 
+            AlertDialog.Builder InformasiAlert = new AlertDialog.Builder(MainActivity.this);
+            InformasiAlert.setTitle("Data Dikonfirmasi");
+            InformasiAlert.setMessage("Hi " + strNama + "!, Data yang kamu kirim terkonfirmasi, Kamu bisa Check Data disini!");
+            InformasiAlert.setPositiveButton("Check", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent formSuccess = new Intent(MainActivity.this, FormSuccess.class);
+                    formSuccess.putExtra("VAR_NAMA", strNama);
+                    formSuccess.putExtra("VAR_NIK", strNIK);
+                    formSuccess.putExtra("VAR_JK", strJK);
+                    formSuccess.putExtra("VAR_DATE", strDate);
+                    formSuccess.putExtra("VAR_ALAMAT", strAlamat);
+                    formSuccess.putExtra("VAR_EMAIL", strEmail);
+                    formSuccess.putExtra("VAR_STATUS", status);
 
-            Intent formSuccess = new Intent(MainActivity.this, FormSuccess.class);
-            formSuccess.putExtra("VAR_NAMA", strNama);
-            formSuccess.putExtra("VAR_NIK", strNIK);
-            formSuccess.putExtra("VAR_JK", strJK);
-            formSuccess.putExtra("VAR_DATE", strDate);
-            formSuccess.putExtra("VAR_ALAMAT", strAlamat);
-            formSuccess.putExtra("VAR_EMAIL", strEmail);
-            formSuccess.putExtra("VAR_STATUS", status);
-
-            startActivity(formSuccess);
+                    startActivity(formSuccess);
+                }
+            });
+            AlertDialog alertDialog = InformasiAlert.create();
+            alertDialog.show();
         }
     }
-
 }
