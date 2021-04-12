3b22719b50574b48f7f7cd615232d20d5e58fbaa
randynetworks <randyramadhan1973@gmail.com>
2021-04-13 06:33:15 +0700

    feat: add spinner

--- app/src/main/java/com/ayoboga/pertemuan34siang/FormSuccess.java
+++ app/src/main/java/com/ayoboga/pertemuan34siang/FormSuccess.java
@@ -22,8 +22,10 @@ public class FormSuccess extends AppCompatActivity {
         String strNama = getIntent().getStringExtra("VAR_NAMA");
         String strNIK = getIntent().getStringExtra("VAR_NIK");
         String strJK = getIntent().getStringExtra("VAR_JK");
+        String strKota = getIntent().getStringExtra("VAR_KOTA");
         String strDate = getIntent().getStringExtra("VAR_DATE");
         String strAlamat = getIntent().getStringExtra("VAR_ALAMAT");
+        String strNegara = getIntent().getStringExtra("VAR_NEGARA");
         String strEmail = getIntent().getStringExtra("VAR_EMAIL");
         Boolean status = getIntent().getBooleanExtra("VAR_STATUS", false);
 
@@ -34,14 +36,16 @@ public class FormSuccess extends AppCompatActivity {
         TextView lblJK = (TextView) findViewById(R.id.jk_success);
         TextView lblTgl = (TextView) findViewById(R.id.tgl_success);
         TextView lblAlamat = (TextView) findViewById(R.id.alamat_success);
+        TextView lblNegara = (TextView) findViewById(R.id.negara_success);
         TextView lblEmail = (TextView) findViewById(R.id.email_success);
         TextView lblStatus = (TextView) findViewById(R.id.status_success);
 
         lblNama.setText("Hi, " + strNama + "!");
         lblNIK.setText("NIK kamu, " + strNIK + ",");
         lblJK.setText("Dengan Jenis Kelamin " + strJK + ",");
-        lblTgl.setText("Dan Tanggal Lahir, " + strDate + ",");
-        lblEmail.setText("Kamu Telah terdaftar Vaksinasi dengan email ini,\n" + strEmail);
+        lblTgl.setText("Dan Tempat Tanggal Lahir, " + strKota + ", " + strDate + ",");
+        lblNegara.setText("Yeyy Kamu Orang " + strNegara);
+        lblEmail.setText("Dan Kamu udah terdaftar Vaksinasi dengan email ini,\n" + strEmail);
         lblAlamat.setText("Vaksinasi akan berada di daerah alamat,\n" + strAlamat);
 
         if (status) {
--- app/src/main/java/com/ayoboga/pertemuan34siang/MainActivity.java
+++ app/src/main/java/com/ayoboga/pertemuan34siang/MainActivity.java
@@ -1,20 +1,21 @@
 package com.ayoboga.pertemuan34siang;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -30,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText displayDate;
     private DatePickerDialog.OnDateSetListener dateSetListener;
+    private String[] listKewarganegaraan = {"-- Pilih kewarganegaraan --", "Indonesia", "Singapore", "Malaysia", "Japan", "Lainnya"};
+
+    String strKotaData;
+    String strNegaraData;
+
+
+
 
 
     @Override
@@ -64,6 +72,40 @@ public class MainActivity extends AppCompatActivity {
         };
 
 
+        Spinner spinnerKota = findViewById(R.id.id_kota);
+        Spinner spinnerNegara = findViewById(R.id.id_negara);
+
+        spinnerKota.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            String item = String.valueOf(spinnerKota.getSelectedItem());
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                strKotaData = (item.equals(spinnerKota.getSelectedItem())) ? "" : parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        final ArrayAdapter<String> dataNegara = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, listKewarganegaraan);
+
+
+        spinnerNegara.setAdapter(dataNegara);
+        spinnerNegara.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            String item = String.valueOf(spinnerNegara.getSelectedItem());
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                strNegaraData = (item.equals(spinnerNegara.getSelectedItem())) ? "" : parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         Button btnSubmit = findViewById(R.id.buttonSubmit);
         btnSubmit.setOnClickListener(new MainActivity.submitForm());
 
@@ -73,16 +115,18 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-
-
             EditText textNama = findViewById(R.id.id_name);
             EditText textEmail = findViewById(R.id.id_email);
             EditText textAlamat = findViewById(R.id.id_alamat);
             EditText textNIK = findViewById(R.id.id_NIK);
             EditText textDate = findViewById(R.id.id_tgl);
+
             RadioGroup radioJK = findViewById(R.id.id_JK);
+
             RadioButton JK = findViewById(radioJK.getCheckedRadioButtonId());
+
             Switch sakit = findViewById(R.id.id_is_Sick);
+
 
             String strNama = textNama.getText().toString();
             String strEmail = textEmail.getText().toString();
@@ -90,6 +134,8 @@ public class MainActivity extends AppCompatActivity {
             String strNIK = textNIK.getText().toString();
             String strDate = textDate.getText().toString();
             String strJK = JK.getText().toString();
+            String strKota = strKotaData;
+            String strNegara = strNegaraData;
             boolean status = sakit.isChecked();
 
 
@@ -105,6 +151,8 @@ public class MainActivity extends AppCompatActivity {
                                     "Data Input Date\t\t\t" + strDate + "\n" +
                                     "Data Input Email\t\t\t" + strEmail + "\n" +
                                     "Data Input Alamat\t\t" + strAlamat + "\n" +
+                                    "Data Input Kota\t\t" + strKota + "\n" +
+                                    "Data Input Negara\t\t" + strNegara + "\n" +
                                      "Data Input Status\t\t" + status
                             , Toast.LENGTH_LONG);
                     data.show();
@@ -119,6 +167,8 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("Data Input Date : " + strDate);
             System.out.println("Data Input Email : " + strEmail);
             System.out.println("Data Input Alamat : " + strAlamat);
+            System.out.println("Data Input Kota : " + strKota);
+            System.out.println("Data Input Negara : " + strNegara);
             System.out.println("Data Input Status : " + status);
 
             AlertDialog.Builder InformasiAlert = new AlertDialog.Builder(MainActivity.this);
@@ -131,8 +181,10 @@ public class MainActivity extends AppCompatActivity {
                     formSuccess.putExtra("VAR_NAMA", strNama);
                     formSuccess.putExtra("VAR_NIK", strNIK);
                     formSuccess.putExtra("VAR_JK", strJK);
+                    formSuccess.putExtra("VAR_KOTA", strKota);
                     formSuccess.putExtra("VAR_DATE", strDate);
                     formSuccess.putExtra("VAR_ALAMAT", strAlamat);
+                    formSuccess.putExtra("VAR_NEGARA", strNegara);
                     formSuccess.putExtra("VAR_EMAIL", strEmail);
                     formSuccess.putExtra("VAR_STATUS", status);
 
