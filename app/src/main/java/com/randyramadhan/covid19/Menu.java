947cf46fef2ca02e7b3c7170f37893b9f1d42bd9
randynetworks <randyramadhan1973@gmail.com>
2021-04-28 13:50:51 +0700

    feat: add 5 alert

--- app/src/main/java/com/randyramadhan/covid19/Menu.java
+++ app/src/main/java/com/randyramadhan/covid19/Menu.java
@@ -1,11 +1,14 @@
 package com.randyramadhan.covid19;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Menu extends Activity {
     @Override
@@ -24,6 +27,12 @@ public class Menu extends Activity {
         Button btnSetting = findViewById(R.id.btn_setting);
         Button btnProject9 = findViewById(R.id.btn_project_9);
         Button btnLain = findViewById(R.id.btn_lain);
+
+        Button btnAlertOk = findViewById(R.id.btn_ok);
+        Button btnAlertYesNo = findViewById(R.id.btn_yes_no);
+        Button btnAlertYesNoCancel = findViewById(R.id.btn_yes_no_cancel);
+        Button btnAlertSingle = findViewById(R.id.btn_single_choice);
+        Button btnAlertMultiple = findViewById(R.id.btn_multiple_choice);
 
 
         btnInput.setOnClickListener(new View.OnClickListener() {
@@ -120,6 +129,167 @@ public class Menu extends Activity {
                 startActivityForResult(lain, 0);
             }
         });
+
+        btnAlertOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(Menu.this);
+                dialog.setTitle("Ini Judul Alert");
+                dialog.setMessage("Ini pesan Info ALert");
+                dialog.setIcon(R.drawable.icon_info);
+
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Menu.this, "Toast dari Alert Ok", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                dialog.setCancelable(false);
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+
+            }
+        });
+
+        btnAlertYesNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(Menu.this);
+                dialog.setTitle("Ini Judul Alert YES NO");
+                dialog.setMessage("Ini pesan Info ALert YES NO");
+                dialog.setIcon(R.drawable.icon_info);
+
+                dialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Menu.this, "Toast dari Alert YES Button", Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Menu.this, "Toast dari Alert NO Button", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                dialog.setCancelable(false);
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+            }
+        });
+
+        btnAlertYesNoCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(Menu.this);
+                dialog.setTitle("Ini Judul Alert YES NO CANCEL");
+                dialog.setMessage("Ini pesan Info ALert YES NO CANCEL");
+                dialog.setIcon(R.drawable.icon_info);
+
+                dialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Menu.this, "Toast dari Alert YES Button", Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Menu.this, "Toast dari Alert NO Button", Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialog.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Menu.this, "Toast dari Alert Cancel Button", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        Toast.makeText(Menu.this, "Toast dari Alert Cancel Diluar", Toast.LENGTH_LONG).show();
+                    }
+                });
+                // Kalo kodisi user jangan cancle di luar
+                 dialog.setCancelable(false);
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+            }
+        });
+
+        btnAlertSingle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(Menu.this);
+                dialog.setTitle("Ini Judul Alert Single Choice");
+                dialog.setIcon(R.drawable.icon_info);
+
+                String[] listBarang = getResources().getStringArray(R.array.val_barang);
+
+                dialog.setSingleChoiceItems(listBarang, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Menu.this, "Item yang kamu pilih: " + listBarang[which].toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                dialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Menu.this, "Toast dari Alert YES Button", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                dialog.setCancelable(false);
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+            }
+        });
+
+        btnAlertMultiple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(Menu.this);
+                dialog.setTitle("Ini Judul Alert Single Choice");
+                dialog.setIcon(R.drawable.icon_info);
+
+                String[] listBarang = getResources().getStringArray(R.array.val_barang);
+                boolean[] checkedItems = {false, false, false, false, false};
+                String[] pilihBarang = {""};
+
+                dialog.setMultiChoiceItems(listBarang, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        checkedItems[which] = isChecked;
+                        pilihBarang[0]= "";
+
+                        for (int i = 0; i < checkedItems.length; i++){
+                            if (checkedItems[i] == true) {
+                                pilihBarang[0] += listBarang[i].toString() + ",";
+                            }
+                        }
+
+                        Toast.makeText(Menu.this, "Item yang kamu pilih: " + pilihBarang[0], Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+                dialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Menu.this, "Toast dari Alert YES Button", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                dialog.setCancelable(false);
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+            }
+        });
+
+
 
 
     }
